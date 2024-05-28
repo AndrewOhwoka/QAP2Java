eb5d454784a37bdb1be25f9f788160467d1b8267
Andrew Ohwoka <150045330+AndrewOhwoka@users.noreply.github.com>
2024-05-28 17:33:52 -0230

    QuestionOneFinal

--- TestMyLine.java
+++ TestMyLine.java
@@ -4,8 +4,8 @@ public class TestMyLine {
     public static void main(String[] args) {
         
         // Create points
-        MyPoint p1 = new MyPoint(1, 2);
-        MyPoint p2 = new MyPoint(4, 6);
+        MyPoint p1 = new MyPoint(2, 4);
+        MyPoint p2 = new MyPoint(6, 8);
 
         // Create a line using the points
         MyLine line1 = new MyLine(p1, p2);
@@ -16,7 +16,7 @@ public class TestMyLine {
         System.out.println("End point: " + line1.getEnd());
 
         line1.setBegin(new MyPoint(0, 0));
-        line1.setEnd(new MyPoint(3, 4));
+        line1.setEnd(new MyPoint(4, 5));
         System.out.println("Updated line: " + line1);
 
         // Test length and gradient
@@ -24,8 +24,8 @@ public class TestMyLine {
         System.out.println("Gradient of line: " + line1.getGradient());
 
         // Test coordinate setters
-        line1.setBeginXY(2, 3);
-        line1.setEndXY(5, 7);
+        line1.setBeginXY(3, 4);
+        line1.setEndXY(6, 8);
         System.out.println("Line after setting coordinates: " + line1);
     }
     }
