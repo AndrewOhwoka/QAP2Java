ce0c5103d44490f3a552a13bb59f458f80104a04
Andrew Ohwoka <150045330+AndrewOhwoka@users.noreply.github.com>
2024-05-28 16:31:42 -0230

    First Commit

---
+++ TestMyLine.java
@@ -0,0 +1,14 @@
+/**
+ * TestMyLine
+ */
+public class TestMyLine {
+
+    public static void main(String[] args) {
+        
+        MyPoint p1 = new MyPoint(3,4);
+        MyPoint p2 = new MyPoint(6,10);
+        
+        // MyLine l1 = new MyLine(p1, p2);
+        // System.out.println(l1);
+    }
+}
